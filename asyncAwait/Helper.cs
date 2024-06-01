aa6d94b9dc1a32cfde5997d80cb8dcf3f02538d0
nvivek42 <viveknarole100@gmail.com>
2024-06-01 13:13:54 +0530

    asyncAwait

---
+++ asyncAwait/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asyncAwait
+{
+    public class Helper
+    {
+      public static async Task<int> performTask()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            });
+            return 9;
+        }
+    }
+}
---
+++ asyncAwait/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using asyncAwait;
+
+Console.WriteLine("Hello, World!");
+
+int result = await Helper.performTask();
+Console.WriteLine("result : "+$"{result}");
+Console.WriteLine("program ends");
